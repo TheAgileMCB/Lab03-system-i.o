4da71224a506b6653c55f37d48d593bc4f2d9c3b
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-06 15:00:19 -0500

    passed test to verify append to txt file

--- SystemIO.Tests/SystemIO.Tests.cs
+++ SystemIO.Tests/SystemIO.Tests.cs
@@ -23,5 +23,26 @@ namespace SystemIO.Tests
             Assert.Equal(new[] { "See Lady Gaga" }, lines);
 
         }
+
+        [Fact]
+        public void Can_append_bucket_list()
+        {
+            // Arrange | Given
+            string path = "BucketList.txt";
+            string[] initialContents = new[] { "See Lady Gaga" };
+
+            File.WriteAllLines(path, initialContents);
+
+            string[] contentsToAppend = new[] { "base jump into a canyon" };
+
+
+            // Act | When
+            File.AppendAllLines(path, contentsToAppend);
+
+            // Assert | Then
+            string[] expectedContents = new[] { "See Lady Gaga", "base jump into a canyon" };
+            string[] actualContents = File.ReadAllLines(path);
+            Assert.Equal(expectedContents, actualContents);
+        }
     }
 }
