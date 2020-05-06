fd5aa38d73805754b6c9224c9d8dcce724e46191
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-06 15:32:42 -0500

    passed test to remove items from the BucketList

--- SystemIO.Tests/SystemIO.Tests.cs
+++ SystemIO.Tests/SystemIO.Tests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace SystemIO.Tests
@@ -17,7 +19,7 @@ namespace SystemIO.Tests
 
 
             // Act | When
-           string[] lines = File.ReadAllLines(path);
+            string[] lines = File.ReadAllLines(path);
 
             // Assert | Then
             Assert.Equal(new[] { "See Lady Gaga" }, lines);
@@ -41,6 +43,33 @@ namespace SystemIO.Tests
 
             // Assert | Then
             string[] expectedContents = new[] { "See Lady Gaga", "base jump into a canyon" };
+            string[] actualContents = File.ReadAllLines(path);
+            Assert.Equal(expectedContents, actualContents);
+        }
+
+        [Fact]
+        public void Can_remove_bucket_list()
+        {
+            // Arrange | Given
+            string path = "BucketList.txt";
+            string[] initialContents = new[] { "See Lady Gaga" };
+
+            File.WriteAllLines(path, initialContents);
+
+            string[] contentsToAppend = new[] { "base jump into a canyon" };
+            File.AppendAllLines(path, contentsToAppend);
+
+
+            // Act | When
+            List<string> quotelist = File.ReadAllLines(path).ToList();
+            string secondItem = quotelist[1];
+            quotelist.RemoveAt(1);
+            File.WriteAllLines(path, quotelist.ToArray());
+           
+
+
+            // Assert | Then
+            string[] expectedContents = new[] { "See Lady Gaga" };
             string[] actualContents = File.ReadAllLines(path);
             Assert.Equal(expectedContents, actualContents);
         }
