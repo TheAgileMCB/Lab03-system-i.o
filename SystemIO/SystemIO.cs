a189d43fae350786bb389f0d9758141de3631950
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-05-06 16:06:02 -0500

    added Main Menu method

--- SystemIO/SystemIO.cs
+++ SystemIO/SystemIO.cs
@@ -6,26 +6,40 @@ namespace SystemIO
     {
         public static void Main(string[] args)
         {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+
+        public static bool MainMenu()
+        {
             Console.WriteLine("THE Bucket List");
-            Console.Write("1. View your adventures \n\n2. Add an adventure \n3. Remove an adventure \n4. Exit \n\n");
+            Console.Write("\nMain Menu \n\n1. View your adventures \n2. Add an adventure \n3. Remove an adventure \n4. Exit \n\n");
             Console.WriteLine("Type the corresponding number to the option you'd like to choose:");
             string userInput = Console.ReadLine();
 
-            if (userInput == "1")
+            switch (userInput)
             {
-                AddAdventure();
-            }
-            else if (userInput == "2")
-            {
-                RemoveAdventure();
-            }
-            else if (userInput == "3")
-            {
-                ViewAdventures();
-            }
-            else
-            {
-               
+                case "1":
+                    ViewAdventures();
+                    return true;
+
+                case "2":
+                    AddAdventure();
+                    return true;
+
+                case "3":
+                    RemoveAdventure();
+                    return true;
+
+                case "4":
+                    Console.WriteLine("Goodbye.");
+                    return false;
+
+                default:
+                    return true;
             }
         }
 
